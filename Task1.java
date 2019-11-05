1aefac464384c59cfeeec8f7afc6e292583859ad
Yehya Eldeeb <yehyapal@gmail.com>
2019-11-05 17:17:18 +0800

    converts tabs to spaces

--- Task1.java
+++ Task1.java
@@ -14,29 +14,29 @@ import lejos.utility.Delay;
 public class Task1 {
 
 
-	private static final int WIDTH = 160;
+    private static final int WIDTH = 160;
     private static final int HEIGHT = 120;
     //private static final int NUM_PIXELS = WIDTH * HEIGHT;
     
     private static int [][] luminanceFrame = new int [HEIGHT][WIDTH];
     private static int threshold = 90;
     private static RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.A);
-	private static RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.D);
-	enum RobotMovement {
-		LEFT,
-		STRAIGHT,
-		RIGHT
-	}
-	private static RobotMovement robotMovement = RobotMovement.STRAIGHT;
+    private static RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.D);
+    enum RobotMovement {
+        LEFT,
+        STRAIGHT,
+        RIGHT
+    }
+    private static RobotMovement robotMovement = RobotMovement.STRAIGHT;
      
     public Task1() {
-		// Initialize luminance frame
-    	for (int x=0; x<WIDTH; x += 1) {
-    		for (int y=0; y<HEIGHT; y += 1) {
-    			luminanceFrame[y][x] = 0;
-    		}
-    	}
-	}
+        // Initialize luminance frame
+        for (int x=0; x<WIDTH; x += 1) {
+            for (int y=0; y<HEIGHT; y += 1) {
+                luminanceFrame[y][x] = 0;
+            }
+        }
+    }
         
     public static void main(String[] args) throws IOException  {
          
@@ -47,8 +47,8 @@ public class Task1 {
         int blackLeft, blackRight;
             
         while (Button.ESCAPE.isUp()) {
-        	blackLeft = 0;
-        	blackRight = 0;
+            blackLeft = 0;
+            blackRight = 0;
             video.grabFrame(frame);
                        
             // Create a frame of luminance values
@@ -64,59 +64,59 @@ public class Task1 {
                     threshold = 255;
             }
             else if (Button.DOWN.isDown()) { 
-            	threshold -=5;
+                threshold -=5;
                 if (threshold < 0)
                     threshold = 0;
             }
             
             for (int y = 0; y <= HEIGHT / 2; y += 2) {
-            	for (int x = WIDTH / 3; x <= WIDTH / 3 + 2; ++x) {
-            		if (luminanceFrame[y][x] < threshold) {
-                		++blackLeft;
-                	}
-            	}
-            	for (int x = WIDTH / 3 * 2 - 2; x <= WIDTH / 3 * 2; ++x) {
-            		if (luminanceFrame[y][x] < threshold) {
-                		++blackRight;
-                	}
-            	} 
+                for (int x = WIDTH / 3; x <= WIDTH / 3 + 2; ++x) {
+                    if (luminanceFrame[y][x] < threshold) {
+                        ++blackLeft;
+                    }
+                }
+                for (int x = WIDTH / 3 * 2 - 2; x <= WIDTH / 3 * 2; ++x) {
+                    if (luminanceFrame[y][x] < threshold) {
+                        ++blackRight;
+                    }
+                } 
             }
             
             //Four Conditions
             if (blackLeft >= blackRight * 0.9 && blackLeft <= blackRight * 1.1 && (blackLeft <= 24)) {
-            	if (robotMovement != RobotMovement.RIGHT) {
-            		turnRight();
-            	}            	
-            	// todo have condition, if this was preceded by
-            	// an all white region for a bit, then turn exactly 
-            	// 180 degrees and follow the path. This means it
-            	// has reached the end of the maze, and the end of
-            	// the white square perimeter.
+                if (robotMovement != RobotMovement.RIGHT) {
+                    turnRight();
+                }                
+                // todo have condition, if this was preceded by
+                // an all white region for a bit, then turn exactly 
+                // 180 degrees and follow the path. This means it
+                // has reached the end of the maze, and the end of
+                // the white square perimeter.
             }
             else if (blackLeft >= blackRight * 0.9 && blackLeft <= blackRight * 1.1) {
-            	if (robotMovement != RobotMovement.STRAIGHT) {
-            		if (robotMovement == RobotMovement.LEFT) {
-            			motorLeft.setSpeed(180);
-            			motorLeft.rotate(70, false);
-            		}
-            		else {
-            			motorRight.setSpeed(180);
-            			motorRight.rotate(70, false);
-            		}
-            		goStraight();
-            	}
+                if (robotMovement != RobotMovement.STRAIGHT) {
+                    if (robotMovement == RobotMovement.LEFT) {
+                        motorLeft.setSpeed(180);
+                        motorLeft.rotate(70, false);
+                    }
+                    else {
+                        motorRight.setSpeed(180);
+                        motorRight.rotate(70, false);
+                    }
+                    goStraight();
+                }
             }
             else if (blackLeft > blackRight) {
-            	if (robotMovement != RobotMovement.RIGHT) {
-            		Delay.msDelay(100);
-            		turnRight();
-            	}
+                if (robotMovement != RobotMovement.RIGHT) {
+                    Delay.msDelay(100);
+                    turnRight();
+                }
             }
             else if (blackLeft < blackRight) {
-            	if (robotMovement != RobotMovement.LEFT) {
-            		Delay.msDelay(100);
-            		turnLeft();
-            	}
+                if (robotMovement != RobotMovement.LEFT) {
+                    Delay.msDelay(100);
+                    turnLeft();
+                }
             }                
         }
         video.close();
@@ -124,57 +124,57 @@ public class Task1 {
     
     
     public static void extractLuminanceValues(byte [] frame) {
-    	int x,y;
-    	int doubleWidth = 2*WIDTH; // y1: pos 0; u: pos 1; y2: pos 2; v: pos 3.
-    	int frameLength = frame.length;
-    	for(int i=0;i<frameLength;i+=2) {
-    		x = (i / 2) % WIDTH;
-    		y = i / doubleWidth;
-    		//luminanceFrame[y][x] = 2*Math.abs((int) frame[i]);
-    		luminanceFrame[y][x] = frame[i] & 0xFF;   		
-    	}
+        int x,y;
+        int doubleWidth = 2*WIDTH; // y1: pos 0; u: pos 1; y2: pos 2; v: pos 3.
+        int frameLength = frame.length;
+        for(int i=0;i<frameLength;i+=2) {
+            x = (i / 2) % WIDTH;
+            y = i / doubleWidth;
+            //luminanceFrame[y][x] = 2*Math.abs((int) frame[i]);
+            luminanceFrame[y][x] = frame[i] & 0xFF;           
+        }
     }
     
     
     public static void dispFrame() {
-    	for (int y=0; y<HEIGHT; y++) {
-    		for (int x=0; x<WIDTH; x++) {
-    			if (luminanceFrame[y][x] <= threshold) {
-    				LCD.setPixel(x, y, 1);
-    			}
-    			else {
-    				LCD.setPixel(x, y, 0);
-    			}	
-    		}
-    	}
+        for (int y=0; y<HEIGHT; y++) {
+            for (int x=0; x<WIDTH; x++) {
+                if (luminanceFrame[y][x] <= threshold) {
+                    LCD.setPixel(x, y, 1);
+                }
+                else {
+                    LCD.setPixel(x, y, 0);
+                }    
+            }
+        }
     }
         
     public static void stop() {
-    	motorRight.stop(true);
-    	motorLeft.stop(true);
+        motorRight.stop(true);
+        motorLeft.stop(true);
     }    
     
     public static void turnRight() {
-    	motorLeft.setSpeed(180);
-		motorRight.setSpeed(45);
-		motorLeft.forward();
-		motorRight.backward();
-		robotMovement = RobotMovement.RIGHT;
+        motorLeft.setSpeed(180);
+        motorRight.setSpeed(45);
+        motorLeft.forward();
+        motorRight.backward();
+        robotMovement = RobotMovement.RIGHT;
     }
        
     public static void turnLeft() {
-    	motorLeft.setSpeed(45); // todo if needed we can multiply by normalisedTimeTurning which will be a variable between 0 and 1
-    	motorRight.setSpeed(180); // try 140, it will turn slower so it'll give the robot more time to react after turning
-    	motorLeft.backward();
-    	motorRight.forward();
-		robotMovement = RobotMovement.LEFT;
+        motorLeft.setSpeed(45); // todo if needed we can multiply by normalisedTimeTurning which will be a variable between 0 and 1
+        motorRight.setSpeed(180); // try 140, it will turn slower so it'll give the robot more time to react after turning
+        motorLeft.backward();
+        motorRight.forward();
+        robotMovement = RobotMovement.LEFT;
     }
     
     public static void goStraight() {
-    	motorLeft.setSpeed(180);
-    	motorRight.setSpeed(180);
-    	motorRight.forward();
-    	motorLeft.forward();
-		robotMovement = RobotMovement.STRAIGHT;
+        motorLeft.setSpeed(180);
+        motorRight.setSpeed(180);
+        motorRight.forward();
+        motorLeft.forward();
+        robotMovement = RobotMovement.STRAIGHT;
     }
 }
